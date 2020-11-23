azerothcore/azerothcore-wotlk

add my 16 gm ranks

src/server/scripts/Commands/cs_tele.cpp
@@ -30,13 +30,13 @@ public:
         {
             { "add",            SEC_ADMINISTRATOR,   false, &HandleTeleAddCommand,             "" },
             { "del",            SEC_ADMINISTRATOR,   true,  &HandleTeleDelCommand,             "" },
-            { "name",           SEC_GAMEMASTER,      true,  &HandleTeleNameCommand,            "" },
-            { "group",          SEC_GAMEMASTER,      false, &HandleTeleGroupCommand,           "" },
+            { "name",           SEC_Fulltester,      true,  &HandleTeleNameCommand,            "" },
+            { "group",          SEC_DONATOR,         false, &HandleTeleGroupCommand,           "" },
             { "",               SEC_MODERATOR,       false, &HandleTeleCommand,                "" }
         };
         static std::vector<ChatCommand> commandTable =
         {
-            { "tele",           SEC_MODERATOR,      false, nullptr,                            "", teleCommandTable }
+            { "tele",           SEC_Fulltester,      false, nullptr,                            "", teleCommandTable }
         };
         return commandTable;
     }

newrage/azerothcore-rage

Signed-off-by: ragestriker <bilndrage66@gmail.com>

src/server/scripts/Commands/cs_tele.cpp
@@ -30,13 +30,13 @@ public:
         {
             { "add",            SEC_ADMINISTRATOR,   false, &HandleTeleAddCommand,             "" },
             { "del",            SEC_ADMINISTRATOR,   true,  &HandleTeleDelCommand,             "" },
-            { "name",           SEC_GAMEMASTER,      true,  &HandleTeleNameCommand,            "" },
-            { "group",          SEC_GAMEMASTER,      false, &HandleTeleGroupCommand,           "" },
+            { "name",           SEC_Fulltester,      true,  &HandleTeleNameCommand,            "" },
+            { "group",          SEC_DONATOR,         false, &HandleTeleGroupCommand,           "" },
             { "",               SEC_MODERATOR,       false, &HandleTeleCommand,                "" }
         };
         static std::vector<ChatCommand> commandTable =
         {
-            { "teleport",           SEC_MODERATOR,      false, nullptr,      "", teleCommandTable }
+            { "tele",           SEC_MODERATOR,      false, nullptr,                            "", teleCommandTable }
         };
         return commandTable;
     }